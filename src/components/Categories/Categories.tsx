72e9ee0773d62a4692c3dab4f45e823cb590d915
mherhay93 <mherhay93@gmail.com>
2022-12-01 14:59:44 +0400

    choose categories

--- src/components/Categories/Categories.tsx
+++ src/components/Categories/Categories.tsx
@@ -1,22 +1,31 @@
 import './styleCategoryes.css'
 import useFetchCategories from "../../apiHook/useDataCategories";
 import {useSelector} from "react-redux";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import {IDataCategories, ITypeCategories} from "../../types/dataCategoriesType";
+import useDataCats from "../../apiHook/useDataCats";
 
 const Categories = () => {
+    const [id, setId] = useState('1')
     const fetchDataCategories = useFetchCategories()
-    
+    const fetchDataCats = useDataCats()
     useEffect(() => {
         fetchDataCategories()
-    }, [])
+        fetchDataCats(id)
+    }, [id])
+    
+    const handleId = (item:ITypeCategories) => {
+        setId(item.id)
+    }
     
     const {dataCategories} = useSelector((state:IDataCategories):any => state.dataCategories)
 
     return (
         <div className='categories_container'>
             {
-                dataCategories[0] && dataCategories[0].map((item:ITypeCategories) => <span key={item.id}>{item.name}</span>)
+                dataCategories[0] && dataCategories[0].map((item:ITypeCategories) => (
+                    <span onClick={() => handleId(item)} className={item.id === id ? 'categories_item_active' : 'categories_item'} key={item.id}>{item.name}</span>
+                ))
             }
         </div>
     )
--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,17 +1,10 @@
 import './styleMain.css'
-import useDataCats from "../../apiHook/useDataCats";
-import {useEffect} from "react";
 import {useSelector} from "react-redux";
 import {IDataCategories, ITypeCats} from "../../types/dataCategoriesType";
 
 const Main = () => {
-    const fetchDataCats = useDataCats()
     const {cats} = useSelector((state:IDataCategories):any => state?.dataCategories)
-    console.log(cats)
     
-    useEffect(() => {
-        fetchDataCats()
-    }, [])
   return (
       <div className='main_container'>
           {cats[0] && cats[0].map((item:ITypeCats)=> <img className='main_items' key={item.id} src={item.url} alt="img"/>)}
